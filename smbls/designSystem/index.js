6044c466282c3aba01446207b5aa025d20877ef0
nikoloza <nika.tomadze@gmail.com>
2026-02-24 07:00:55 +0400

    Initial commit

---
+++ smbls/designSystem/index.js
@@ -0,0 +1,43 @@
+import color from './COLOR.js'
+import gradient from './GRADIENT.js'
+import theme from './THEME.js'
+import font from './FONT.js'
+import fontFamily from './FONT_FAMILY.js'
+import typography from './TYPOGRAPHY.js'
+import spacing from './SPACING.js'
+import timing from './TIMING.js'
+import classes from './CLASS.js'
+import grid from './GRID.js'
+import icons from './ICONS.js'
+import shape from './SHAPE.js'
+import reset from './RESET.js'
+import animation from './ANIMATION.js'
+import media from './MEDIA.js'
+import cases from './CASES.js'
+
+export default {
+  COLOR: color,
+  GRADIENT: gradient,
+  THEME: theme,
+  FONT: font,
+  FONT_FAMILY: fontFamily,
+  TYPOGRAPHY: typography,
+  SPACING: spacing,
+  TIMING: timing,
+  CLASS: classes,
+  GRID: grid,
+  ICONS: icons,
+  SHAPE: shape,
+  RESET: reset,
+  ANIMATION: animation,
+  MEDIA: media,
+  CASES: cases,
+  useReset: true,
+  useVariable: true,
+  useFontImport: true,
+  useIconSprite: true,
+  useSvgSprite: true,
+  useDefaultConfig: true,
+  useDocumentTheme: true,
+  verbose: false
+}
